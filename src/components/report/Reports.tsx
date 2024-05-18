a58ae5d4a3f57172bf654b158cf74b39d35b1522
Yared04 <yaredt29@gmail.com>
2024-05-18 03:09:10 +0300

    fix: restructed home page

--- src/components/report/Reports.tsx
+++ src/components/report/Reports.tsx
@@ -14,15 +14,16 @@ const Reports = () => {
   return (
    <>
    <h1>Reports</h1>
-    <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr", gap: "20px" }}>
-      {hasPermission(PERMISSIONS.GenerateCustomerAgingReport) && <CustomerAgingReportGenerator />}
+    <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr", gap: "20px" }}>      
       {hasPermission(PERMISSIONS.GenerateAPAgingReport) && <ApAgingReportGenerator />}
-      {hasPermission(PERMISSIONS.GenerateBankTransactionReport) && <BankTransactionReportGenerator />}
-      {hasPermission(PERMISSIONS.GenerateTransactionWithSplitSummary) && <TransactionWithSplitSummary />}
+      {hasPermission(PERMISSIONS.GenerateCustomerAgingReport) && <CustomerAgingReportGenerator />}
       {hasPermission(PERMISSIONS.GenerateTrialBalanceReport) && <TrialBalanceReportGenerator />}
       {hasPermission(PERMISSIONS.GenerateInventoryValuationReport) && <InventoryValuationReportGenerator />}
       {hasPermission(PERMISSIONS.GenerateProfitAndLossReport) && <ProfitAndLossSummary />}
-      {hasPermission(PERMISSIONS.GenerateBalanceSheetReport) && <BalanceSheetSummary />}
+      {hasPermission(PERMISSIONS.GenerateBalanceSheetReport) && <BalanceSheetSummary />}      
+      {hasPermission(PERMISSIONS.GenerateTransactionWithSplitSummary) && <TransactionWithSplitSummary />}
+      {hasPermission(PERMISSIONS.GenerateBankTransactionReport) && <BankTransactionReportGenerator />}
+
     </div>
    </>
   );
